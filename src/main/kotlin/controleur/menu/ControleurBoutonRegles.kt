a2397240813d9546ef8b4b20fe498d0c5e635672
E222451U <alexis.lambert1@etu.univ-nantes.fr>
2023-06-21 15:19:23 +0200

    Update

--- src/main/kotlin/controleur/menu/ControleurBoutonRegles.kt
+++ src/main/kotlin/controleur/menu/ControleurBoutonRegles.kt
@@ -7,16 +7,18 @@ import javafx.scene.image.Image
 import javafx.stage.Stage
 import vue.VueRegles
 
-class ControleurBoutonRegles(private val stage: Stage) : EventHandler<ActionEvent> {
+class ControleurBoutonRegles(
+    private val sceneMenu: Scene,
+    private val sceneRegles: Scene,
+    private val vueRegles: VueRegles,
+    private val stage: Stage
+) : EventHandler<ActionEvent> {
 
     override fun handle(event: ActionEvent) {
         stage.close()
-        val sceneRegle = Scene(VueRegles())
-        stage.minHeight = 700.0
-        stage.minWidth = 700.0
-        stage.height = 700.0
-        stage.width = 700.0
-        stage.scene = sceneRegle
+        vueRegles.resetAffichage()
+        stage.minHeight = 739.0
+        stage.scene = sceneRegles
         stage.title = "Règles - Pickomino"
         stage.icons[0] = Image("images/rules_icon.png")
         stage.show()
